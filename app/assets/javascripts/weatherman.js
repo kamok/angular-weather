927392a88590ce8ab81932851fb9a67b137cd363
Ka_Lenovo <heykamok@gmail.com>
2016-06-15 23:58:20 -0400

    Learning binding.

--- app/assets/javascripts/weatherman.js
+++ app/assets/javascripts/weatherman.js
@@ -1,1 +1,9 @@
-angular.module('weatherApp', []);
+var weatherman = angular.module('weatherApp', []);
+
+weatherman.factory('weatherService', function () {
+	return {temperature: "90"}
+})
+
+function WeatherCtrl($scope, weatherService) {
+	$scope.data = weatherService
+}
