e79ce3ba3db835b2b41ab4b226c9bb282c93d55f
Ka_Lenovo <heykamok@gmail.com>
2016-06-17 22:21:42 -0400

    Add weather factory call

--- app/assets/javascripts/weatherman.js
+++ app/assets/javascripts/weatherman.js
@@ -1,4 +1,24 @@
 angular.module('myApp', [])
 	.controller('FirstCtrl', function($scope) {
 		$scope.data = {message: 'Hello!', message2: 'Hello2!'};
+	
+	app.factory('myApp', ['$http', '$q' function ($http, $q){
+		function getWeather (zip) {
+			var deferred = $q.defer();
+      $http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%22' + zip + '%22&format=json&diagnostics=true&callback=')
+      	.success(function(data)){
+      		deferred.resolve(data.query.results.channel);
+      	})
+				.error(function(err)){
+					console.log('Error retrieving data');
+					deferred.reject(err);
+				});
+			return deferred.promise;
+		}
+
+		return {
+			getWeather: getWeather
+		};
+	}]);
+
 	});
