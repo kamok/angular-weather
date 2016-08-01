815e1a498c7a857ded8b9b1f83d41e42af24d469
Ka_Lenovo <heykamok@gmail.com>
2016-07-31 20:06:14 -0400

    Showing forecast, baby

--- app/assets/javascripts/weatherman.js
+++ app/assets/javascripts/weatherman.js
@@ -18,9 +18,17 @@ app.factory('weatherService', ['$http', '$q',
 	  var getWeather = function(geocode) {
 	  	var lat = geocode.lat;
 	  	var lng = geocode.lng;
-	  	console.log(lat);
-	  	console.log(lng);
-	  }
+	  	var deferred = $q.defer();
+	    $http.get('https://api.forecast.io/forecast/55017c0ce854ceab1db44c34d7bb07d5/' + lat + ',' + lng)
+	      .success(function(data){
+	        deferred.resolve(data);
+	      })
+	      .error(function(err){
+	        console.log('Error retrieving markets');
+	        deferred.reject(err);
+	      });
+	    return deferred.promise;
+	  };
 
 		return {
 	 		getGeoCode: getGeoCode,
@@ -31,47 +39,17 @@ app.factory('weatherService', ['$http', '$q',
 app.controller("weatherCtrl", ['$scope','weatherService',
  function($scope, weatherService) {
 	function fetchWeather(zip) {
-		weatherService.getGeoCode(zip).then(function(data){
-			var geocode = data.results[0].geometry.location;
-		weatherService.getWeather(geocode);
+		weatherService.getGeoCode(zip).then(function(geodata){
+			var geocode = geodata.results[0].geometry.location;
+		weatherService.getWeather(geocode).then(function(weatherData){
+			$scope.currentTemp = weatherData.currently.temperature;
+			$scope.zip = zip;
+			// console.log(weatherData.currently.temperature);
+		});
 		});
 	};
-	// $scope.findWeather = function(zip) {
- //    $scope.place = '';
- //    fetchWeather(zip);
- //  };
+	$scope.findWeather = function(zip) {
+    fetchWeather(zip);
+  };
   fetchWeather('10038');
 }]);
-
-
-// app.controller("weatherCtrl", ['$scope','weatherService'], function($scope, weatherService) {
-// 	// function fetchWeather(zip) {
-// 	// 	weatherService.getGeoCode(zip).then(function(data){
-// 	// 		$scope.place = data;
-// 	// 	});
-// 	// };
-// 	// fetchWeather('10038');
-// 	// $scope.findWeather = function(zip) {
-//  //    $scope.place = '';
-//  //    fetchWeather(zip);
-//  //  };
-// });
-
-// app.factory('weatherService', ['$http', '$q', function ($http, $q) {
-// 	function getWeather (zip) {
-//     var deferred = $q.defer();
-//     $http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%22' + zip + '%22&format=json&diagnostics=true&callback=')
-//       .success(function(data){
-//         deferred.resolve(data.query.results.channel);
-//       })
-//       .error(function(err){
-//         console.log('Error retrieving markets');
-//         deferred.reject(err);
-//       });
-//     return deferred.promise;
-//       }
-
-// 	return {
-//  		getWeather: getWeather
-// 	};
-// }]);
