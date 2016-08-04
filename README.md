b93d6d1a5b096136bb9a016411ccacddf07a3917
Ka Mok <heykamok@gmail.com>
2016-08-04 13:37:42 -0400

    Try a few config for Angular, CORS still f-ing me in the a

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # angular-weather
-
-Todo:
-
-Implement this app inside heykamok.com
-"Fun" mode, Bruce Almighty Button?
-Google PlaceAutoComplete
-Is there a maps API that returns an image per query?
-Write tests
--- app/assets/javascripts/weatherman/weather.factory.js
+++ app/assets/javascripts/weatherman/weather.factory.js
@@ -1,5 +1,14 @@
 angular
 	.module("weatherApp")
+  // .config(['$httpProvider', function($httpProvider) {
+  //   $httpProvider.defaults.withCredentials = true;
+  // }])
+  // .config(['$httpProvider', function($httpProvider) {
+  //       $httpProvider.defaults.useXDomain = true;
+  //       $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+  //       delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  //   }
+  // ])
 	.factory('weatherService', ['$http', '$q', 
 	
 function ($http, $q) {
